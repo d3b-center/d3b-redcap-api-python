923b2e36cb40fa560cb30e0e71ffb3dcd0fa2524
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-11-08 17:21:49 -0500

    :bug::recycle::memo: Fix things and document

--- redcap.py
+++ redcap.py
@@ -11,11 +11,11 @@ def _undefault_dict(d):
     return d
 
 
-class RedcapError(Exception):
+class REDCapError(Exception):
     pass
 
 
-class RedcapStudy:
+class REDCapStudy:
     """
     Note to future developers: This class uses get_ and set_ methods on purpose
     to control the user experience. Please don't replace them with property
@@ -30,19 +30,28 @@ class RedcapStudy:
         self.api = api_url
         self.api_token = api_token
 
-    def _get_response(self, type, extra_args=None):
-        params = {
+    def _get_response(self, content, params=None, **kwargs):
+        """API request implementation
+
+        :param content: What kind of content we're requesting to get or set
+        :param params: additional parameters to send
+        :raises REDCapError: REDCap returned an error status
+        :return: REDCap server requests.Response object
+        """
+        all_params = {
             "token": self.api_token,
-            "content": type,
+            "content": content,
             "format": "json",
             "returnFormat": "json",
         }
-        params.update(extra_args or {})
-        if "data" in params and not isinstance(params["data"], str):
-            params["data"] = json.dumps(params["data"])
-        resp = Session().post(self.api, data=params)
+        all_params.update(params or {})
+        if "data" in all_params and not isinstance(all_params["data"], str):
+            all_params["data"] = json.dumps(all_params["data"])
+        all_params = {k: v for k, v in all_params.items() if v is not None}
+        resp = Session().post(self.api, data=all_params, **kwargs)
+        breakpoint()
         if resp.status_code != 200:
-            raise RedcapError(f"HTTP {resp.status_code} - {resp.json()}")
+            raise REDCapError(f"HTTP {resp.status_code} - {resp.text}")
         return resp
 
     def _get_json(self, *args, **kwargs):
@@ -51,71 +60,161 @@ class RedcapStudy:
     def _get_text(self, *args, **kwargs):
         return self._get_response(*args, **kwargs).text
 
-    def get_arms(self):
+    def get_arm_names(self):
+        """Export Arm names
+
+        :return: list of dicts with Arm numbers and names
+        """
+        # https://redcap.chop.edu/api/help/?content=exp_arms
         return self._get_json("arm")
 
-    def set_arms(self, arms, delete_all_first=True):
+    def set_arm_names(self, arms, delete_all_first=True):
+        """Import Arm names or rename existing Arms
+
+        :param delete_all_first: erase all existing Arms first before importing
+        :return: number of Arms imported
+        """
+        # https://redcap.chop.edu/api/help/?content=imp_arms
         return self._get_json(
             "arm",
-            extra_args={
+            params={
                 "data": arms,
                 "override": "1" if delete_all_first else "0",
                 "action": "import",
             },
         )
 
-    def get_events(self):
+    def get_event_metadata(self):
+        """Export Event details (names, numbers, labels, offsets)
+
+        :return: list of dicts with Event details
+        """
+        # https://redcap.chop.edu/api/help/?content=exp_metadata
         return self._get_json("event")
 
-    def set_events(self, events, delete_all_first=True):
+    def set_event_metadata(self, events, delete_all_first=True):
+        """Import Event details (names, numbers, labels, offsets)
+
+        :param events: see get_event_metadata return
+        :param delete_all_first: erase all existing Events first before importing
+        :return: number of Events imported
+        """
+        # https://redcap.chop.edu/api/help/?content=imp_metadata
         return self._get_json(
             "event",
-            extra_args={
+            params={
                 "action": "import",
                 "override": "1" if delete_all_first else "0",
             },
         )
 
     def get_field_export_names(self):
+        """Export mappings of field names and selected values to exported names
+
+        :return: list of dicts of field choices
+        """
+        # https://redcap.chop.edu/api/help/?content=exp_field_names
         return self._get_json("exportFieldNames")
 
     def _act_file(
-        self, action, record, field, event=None, repeat_instance=None
+        self,
+        action,
+        record,
+        field,
+        event=None,
+        repeat_instance=None,
+        file_data=None,
     ):
-        args = {"action": action, "record": record, "field": field}
-        if event is not None:
-            args["event"] = event
-        if repeat_instance is not None:
-            args["repeat_instance"] = repeat_instance
-        return self._get_response("file", args)
+        params = {
+            "action": action,
+            "record": record,
+            "field": field,
+            "event": event,
+            "repeat_instance": repeat_instance,
+        }
+        return self._get_response("file", params, files=file_data)
 
     def get_file(self, record, field, event=None, repeat_instance=None):
+        """Export a File from a file upload field on a record
+
+        :param record: the record ID the file is attached to
+        :param field: the name of field with the file
+        :param event: event name if longitudinal
+        :param repeat_instance: which instance if instrument/event is repeating
+        :return: dict with "filename" str and "body" bytes
+        """
+        # https://redcap.chop.edu/api/help/?content=exp_file
         resp = self._act_file("export", record, field, event, repeat_instance)
         file_name = (
-            resp.headers["Content-Type"].split('name="')[1].split('";')[0]
+            unquote(
+                resp.headers["Content-Type"].split('name="')[1].split('";')[0]
+            )
+            .encode("latin1")
+            .decode("utf-8")
         )
-        file_name = unquote(file_name).encode("latin1").decode("utf-8")
         return {"body": resp.content, "filename": file_name}
 
-    def set_file(self, record, field, event=None, repeat_instance=None):
-        return self._act_file(
-            "import", record, field, event, repeat_instance
-        ).json()
+    def set_file(
+        self,
+        filename,
+        file_obj,
+        record,
+        field,
+        event=None,
+        repeat_instance=None,
+    ):
+        """Attach a File to a file upload field on a record
+
+        :param filename: name of the file to create
+        :param file_obj: contents of or read object for the file
+        :param record: the record ID the file is attached to
+        :param field: the name of field with the file
+        :param event: event name if longitudinal
+        :param repeat_instance: which instance if instrument/event is repeating
+        """
+        # https://redcap.chop.edu/api/help/?content=imp_file
+        self._act_file(
+            "import",
+            record,
+            field,
+            event,
+            repeat_instance,
+            {"file": (filename, file_obj)},
+        )
 
     def delete_file(self, record, field, event=None, repeat_instance=None):
-        return self._act_file(
-            "delete", record, field, event, repeat_instance
-        ).json()
+        """Delete a File from a file upload field on a record
+
+        :param record: the record ID the file is attached to
+        :param field: the name of field with the file
+        :param event: event name if longitudinal
+        :param repeat_instance: which instance if instrument/event is repeating
+        """
+        # https://redcap.chop.edu/api/help/?content=del_file
+        self._act_file(
+            action="delete",
+            record=record,
+            field=field,
+            event=event,
+            repeat_instance=repeat_instance,
+        )
 
     def get_redcap_version(self):
+        """Get the version of REDCap as a string"""
+        # https://redcap.chop.edu/api/help/?content=exp_rc_v
         return self._get_text("version")
 
     def get_project_info(self):
+        """Get basic project attributes such as title, logitudinality,
+        if surveys are enabled, creation time, etc."""
         return self._get_json("project")
 
     def set_project_info(self, project_info):
+        """Set basic project attributes such as title, logitudinality,
+        if surveys are enabled, creation time, etc."""
+        # https://redcap.chop.edu/api/help/?content=imp_proj_sett
         return self._get_json(
-            "project_settings", extra_args={"data": project_info}
+            "project_settings", params={"data": project_info}
         )
 
     def get_project_xml(
@@ -125,6 +224,18 @@ class RedcapStudy:
         include_survey_fields=True,
         include_files=True,
     ):
+        """Fetch the entire project as a special XML file in CDISC ODM format.
+
+        :param metadata_only: Don't include any of the record data
+        :param include_data_access_groups: Include the redcap_data_access_group
+            field in data (does nothing if metadata_only is True)
+        :param include_survey_fields: Include survey identifier fields in data
+            (does nothing if metadata_only is True)
+        :param include_files: Include file_upload and signature fields in data
+            (does nothing if metadata_only is True)
+
+        :return: string contents of an XML file
+        """
         return self._get_text(
             "project_xml",
             {
@@ -136,16 +247,28 @@ class RedcapStudy:
         )
 
     def get_users(self):
+        """List user information with privileges, email address, and names"""
         return self._get_json("user")
 
     def set_users(self, users):
+        """Set or update user privileges, email address, and names
+
+        :param users: see output of get_users
+        :return: number of users added or updated
+        """
         return self._get_json("user", {"data": users})
 
-    def get_metadata(self):
+    def get_data_dictionary(self):
+        """Get the instrument definition information."""
         return self._get_json("metadata")
 
-    def set_metadata(self, metadata):
-        return self._get_json("metadata", {"data": metadata})
+    def set_data_dictionary(self, data_dictionary):
+        """Set the instrument definitions.
+
+        :param data_dictionary: see output of get_data_dictionary
+        :return: number of fields imported
+        """
+        return self._get_json("metadata", {"data": data_dictionary})
 
     def get_instrument_event_mappings(self):
         return self._get_json("formEventMapping")
@@ -193,15 +316,15 @@ class RedcapStudy:
         raw=True,
         raw_headers=True,
         checkbox_labels=False,
-        extra_args=None,
+        params=None,
     ):
         args = {
             "rawOrLabel": "raw" if raw else "label",
             "rawOrLabelHeaders": "raw" if raw_headers else "label",
             "exportCheckboxLabel": "true" if checkbox_labels else "false",
         }
-        args.update(extra_args or {})
-        return self._get_json(content, extra_args=args)
+        args.update(params or {})
+        return self._get_json(content, params=args)
 
     def get_records(
         self,
@@ -217,7 +340,7 @@ class RedcapStudy:
             raw=raw,
             raw_headers=raw_headers,
             checkbox_labels=checkbox_labels,
-            extra_args={
+            params={
                 "type": type,
                 "exportSurveyFields": "true" if survey_fields else "false",
                 "exportDataAccessGroups": "true"
@@ -237,13 +360,13 @@ class RedcapStudy:
         }
         if auto_number:
             args["returnContent"] = "auto_ids"
-        self._get_json("record", extra_args=args)
+        self._get_json("record", params=args)
 
     def get_repeating_forms_events(self):
         self._get_json("repeatingFormsEvents")
 
     def set_repeating_forms_events(self, rfe):
-        self._get_json("repeatingFormsEvents", extra_args={"data": rfe})
+        self._get_json("repeatingFormsEvents", params={"data": rfe})
 
     def get_report_records(
         self, report_id, raw=True, raw_headers=True, checkbox_labels=False
@@ -253,7 +376,7 @@ class RedcapStudy:
             raw=raw,
             raw_headers=raw_headers,
             checkbox_labels=checkbox_labels,
-            extra_args={"report_id": report_id},
+            params={"report_id": report_id},
         )
 
     def get_selector_choice_map(self):
