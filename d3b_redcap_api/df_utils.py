cfab8bc5ffad64bd0d32b020f7858443e60d277f
Avi Kelman <patcherton.fixesthings@gmail.com>
2021-03-24 14:35:06 -0600

    :sparkles: Add utility function for converting all instruments to dfs

--- d3b_redcap_api/df_utils.py
+++ d3b_redcap_api/df_utils.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from numpy import nan
 from pandas import DataFrame
 
@@ -6,14 +8,14 @@ def to_df(records_tree, event_name, instrument_name):
     """
     Converts a `get_records_tree()[event][instrument]` to a pandas DataFrame
     """
-    # get records
-    instrument = records_tree[event_name][instrument_name]
+    # get instrument records
+    records = records_tree[event_name][instrument_name]
 
-    # convert records to dataframe rows
+    # build dataframe rows from records
     acc = []
-    for p, es in instrument.items():
-        for i, e in es.items():
-            entry = {"subject": p, f"subject_{instrument_name}_instance": i}
+    for s, entries in records.items():
+        for i, e in entries.items():
+            entry = {"subject": s, f"subject_{instrument_name}_instance": i}
             entry.update({k: "+".join(sorted(v)) for k, v in e.items()})
             acc.append(entry)
     df = DataFrame(acc)
@@ -26,6 +28,23 @@ def to_df(records_tree, event_name, instrument_name):
     }
     df = df.dropna(how="all", subset=df.columns.difference(unimportant))
     return df.replace(nan, "").astype(str)
+
+
+def all_dfs(records_tree):
+    """
+    Calls to_df on every instrument found in the records tree and returns a
+    dict keyed by the instrument name if the instrument name is unique or
+    by event_instrument if not.
+    """
+    names = [i for instruments in records_tree.values() for i in instruments]
+    reused = {k for k, v in Counter(names).items() if v > 1}
+
+    dfs = {}
+    for e, event_instruments in records_tree.items():
+        for i in event_instruments:
+            dfs[f"{e}_{i}" if i in reused else i] = to_df(records_tree, e, i)
+
+    return dfs
 
 
 def summary_column(df, from_col_names, separator):
